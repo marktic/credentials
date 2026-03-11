578de617071bef77dade85dafdabc0e254407683
Gabriel Solomon <hello@gabrielsolomon.ro>
2026-03-11 12:16:55 +0200

    feat: update parent card title and add view button action in validate-parent.php

--- src/Bundle/Modules/Admin/views/mkt_credentials-submissions/validate.php
+++ src/Bundle/Modules/Admin/views/mkt_credentials-submissions/validate.php
@@ -27,18 +27,12 @@ $skip = $this->skip ?? [];
     <div class="d-grid gap-3">
         <div class="row">
             <div class="col-md-8">
-                <?= $this->load('/mkt_credentials-submissions/modules/panels/validate-requirement', ['credentialRequirement' => $credentialRequirement]); ?>
-            </div>
-            <div class="col-md-4">
-                <?= $this->load('/mkt_credentials-submissions/modules/panels/validate-parent', ['credentialParent' => $credentialParent]); ?>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-8">
                 <?= $this->load('/mkt_credentials-submissions/modules/panels/validate-file'); ?>
             </div>
             <div class="col-md-4">
+                <?= $this->load('/mkt_credentials-submissions/modules/panels/validate-requirement', ['credentialRequirement' => $credentialRequirement]); ?>
                 <?= $this->load('/mkt_credentials-submissions/modules/panels/validate-actions'); ?>
+                <?= $this->load('/mkt_credentials-submissions/modules/panels/validate-parent', ['credentialParent' => $credentialParent]); ?>
         </div>
     </div>
 <?php endif; ?>
